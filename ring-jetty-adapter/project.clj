60081f9005724bd98276055f5b6cbc9cacc773a6
Ivan Toshkov <ivan@toshkov.org>
2011-02-12 19:18:49 +0200

    Jetty 7

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -3,6 +3,5 @@
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "0.3.5"]
                  [ring/ring-servlet "0.3.5"]
-                 [org.mortbay.jetty/jetty "6.1.26"]
-                 [org.mortbay.jetty/jetty-util "6.1.26"]]
+                 [org.eclipse.jetty/jetty-server "7.2.2.v20101205"]]
   :dev-dependencies [[clj-http "0.1.1"]])
